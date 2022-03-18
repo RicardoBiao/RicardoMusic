823194114275bc7cc2491e8362dfc1dd6dfcce12
Ricardo <liweibiao@kungeek.com>
2022-03-18 18:15:47 +0800

    chore: 添加vueuse

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: liweibiao
+ * @Date: 2022-03-18 17:33:46
+ * @LastEditTime: 2022-03-18 18:06:56
+ * @LastEditors: liweibiao
+ * @Description: 
+ */
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -5,6 +12,7 @@ import './app.scss'
 
 const App = createApp({
   onShow(options) {
+    console.log('app-options: ',options)
   },
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 })
